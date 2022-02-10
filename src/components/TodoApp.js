b73e3e7abae550ef94d99a1f32940db110e4d5dd
khoa2520 <86915350+khoa2520@users.noreply.github.com>
2022-02-10 11:10:23 +0700

    update todolisst

---
+++ src/components/TodoApp.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Header from "./layout/Header";
+import Todos from "./Todos";
+
+class TodoApp extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Deloy to live server",
+        completed: false,
+      },
+    ],
+  };
+  handleCheckboxChange = (id) => {
+    console.log("clicked on checkbox with id = " + id);
+  };
+  render() {
+    return (
+      <div className="container">
+        <Header />
+
+        <Todos
+          todos={this.state.todos}
+          handleChange={this.handleCheckboxChange}
+        />
+
+        {/* <ul>
+          {this.state.todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul> */}
+      </div>
+    );
+  }
+}
+
+// render() {
+//     return (
+//         <div>
+//             <Header />
+//             <Todos todos = {this.state.todos}/>
+//         </div>
+//     )
+// }
+
+export default TodoApp;
